98d62d82020ac11315019774b7e2ad15a0b082e1
Pam <pam.dong@nbcuni.com>
2020-01-14 16:41:44 -0500

    分享图片问题

--- pages/index/index.js
+++ pages/index/index.js
@@ -566,6 +566,39 @@ Page({
       Poster.create(true);
     });
   },
+
+  clipImage : (index, content, cb) => {  
+    var filePath = wx.env.USER_DATA_PATH;
+    console.log(filePath)
+    var shareImg = "../../images/shareImg.jpeg";
+    // ‘canvas’为前面创建的canvas标签的canvas-id属性值
+    let ctx = wx.createCanvasContext('canvas' + index);  
+    let canvasW = 640, canvasH = 512;
+  
+    // 将图片绘制到画布
+    ctx.drawImage(shareImg, 0, 0, canvasW, canvasH, 0, 0, canvasW, canvasH) 
+    // draw()必须要用到，并且需要在绘制成功后导出图片
+    var that = this;
+    ctx.draw(false, () => {
+      setTimeout(() => {
+        //  导出图片
+        wx.canvasToTempFilePath({
+          width: canvasW,
+          height: canvasH,
+          destWidth: canvasW,
+          destHeight: canvasH,
+          canvasId: 'canvas' + index,
+          fileType: 'jpg',
+          success: (res) => {
+            // res.tempFilePath为导出的图片路径
+            typeof cb == 'function' && cb(res.tempFilePath);
+            content.shareImg = res.tempFilePath
+          }
+        }, that)
+      }, 300);
+    })
+  },
+
   getDakaList: function (url) {
     var that = this;
     wx.request({
@@ -575,9 +608,18 @@ Page({
         "Content-Type": "application/json"
       },
       success: function (res) {
-        console.log(res.data.data.records);
-        // var records=res.data.data.records;
         var totalPosts = [];
+        console.log(res.data.data.records);
+        for(var i = 0; i < res.data.data.records.length; i++) {
+          that.clipImage(i, res.data.data.records[i], (img) => {
+            // console.log(img);  // img为最终裁剪后生成的图片路径，我们可以用来做为转发封面图
+            // res.data.data.records[i].shareImg = img
+            that.setData({
+              totalPosts: res.data.data.records
+            })
+          }); 
+        }
+        console.log(res.data.data.records)
         console.log(that.data.isEmpty)
         if (!that.data.isEmpty) {
           totalPosts = that.data.postList.concat(res.data.data.records);
@@ -769,15 +811,31 @@ Page({
     this.getDakaList(nextUrl);
   },
 
+  shareClock: function() {
+    this.onShareAppMessage
+  },
+
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function (res) {
     var postItem = res.target.dataset.value;
-    console.log(postItem)
-    postItem.headPortrait = encodeURIComponent(postItem.headPortrait);
+    console.log(res.target.dataset);
     var that = this;
+    if(postItem.image) {
+      that.setData({
+        picUrl: postItem.image
+      });
+    } else {
+      that.setData({
+        picUrl: postItem.shareImg ? postItem.shareImg : "/images/shareBac.jpeg"
+      });
+      
+    }
+    postItem.headPortrait = encodeURIComponent(postItem.headPortrait);
     var shareObj = {
+      title: postItem.content,
+      imageUrl: that.data.picUrl,
       path: '/pages/article/article?cat=' + JSON.stringify(postItem),
       success: function (res) {
         wx.showToast({
